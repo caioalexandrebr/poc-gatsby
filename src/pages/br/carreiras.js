91c9ca14f3398b371e383b16701dc7bd74fac97d
Caio Alexandre <contato@caioalexandre.com>
2021-05-04 10:34:40 -0300

    first commit

---
+++ src/pages/br/carreiras.js
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Rota BR/Carreiras</h1>
+    </div>
+  )
+}
+
+export default IndexPage
---
+++ src/pages/br/index.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+import { StaticImage } from "gatsby-plugin-image"
+
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Rota BR aaa</h1>
+      <StaticImage src="https://site.gympass.com/documents/514034/0/shutterstock_382697113.jpg" alt="Imagem" />
+    </div>
+  )
+}
+
+export default IndexPage
